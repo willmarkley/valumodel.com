aa9819baadbe7d864321b3696e952bb938fe80f5
Will Markley <willjmarkley@gmail.com>
2016-12-01 15:25:15 -0500

    fixed python syntax errors

--- scripts/dcf.py
+++ scripts/dcf.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append('/var/www/html/valumodel.com/scripts')
 from calc_dcf import calc_dcf
 
-def create_dcf(req, tax_rate, growth_rate_1_year_out, sga_%_of_sales, d&a_%_of_sales, capex_%_of_sales, nwc_%_of_sales, exit_multiple):
+def create_dcf(req, tax_rate, growth_rate_1_year_out, sga_of_sales, da_of_sales, capex_of_sales, nwc_of_sales, exit_multiple):
 	assumptions = {}
 	assumptions['Tax Rate'] = tax_rate/100.0
 	assumptions['Growth Rate 1 year out'] = growth_rate_1_year_out/100.0
