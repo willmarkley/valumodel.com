80bda9f9dfa1204a3151d2aaecbe7e8253dc47dd
Will Markley <willjmarkley@gmail.com>
2016-12-01 15:17:47 -0500

    accounted for percentage

--- scripts/dcf.py
+++ scripts/dcf.py
@@ -6,12 +6,12 @@ from calc_dcf import calc_dcf
 
 def create_dcf(req, tax_rate, growth_rate_1_year_out, sga_%_of_sales, d&a_%_of_sales, capex_%_of_sales, nwc_%_of_sales, exit_multiple):
 	assumptions = {}
-	assumptions['Tax Rate'] = tax_rate
-	assumptions['Growth Rate 1 year out'] = growth_rate_1_year_out
-	assumptions['SGA % of sales'] = sga_%_of_sales
-	assumptions['D&A % of sales'] = d&a_%_of_sales
-	assumptions['CAPEX % of sales'] = capex_%_of_sales
-	assumptions['NWC % of sales'] = nwc_%_of_sales
+	assumptions['Tax Rate'] = tax_rate/100.0
+	assumptions['Growth Rate 1 year out'] = growth_rate_1_year_out/100.0
+	assumptions['SGA % of sales'] = sga_%_of_sales/100.0
+	assumptions['D&A % of sales'] = d&a_%_of_sales/100.0
+	assumptions['CAPEX % of sales'] = capex_%_of_sales/100.0
+	assumptions['NWC % of sales'] = nwc_%_of_sales/100.0
 	assumptions['Exit Multiple'] = exit_multiple
 
 	return calc_dcf()
