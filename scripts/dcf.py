ec7b8f45c56874238fba5314ce2fe612e571c01b
Will Markley <willjmarkley@gmail.com>
2016-12-01 15:31:52 -0500

    Fixed dcf.py typos

--- scripts/dcf.py
+++ scripts/dcf.py
@@ -8,10 +8,10 @@ def create_dcf(req, tax_rate, growth_rate_1_year_out, sga_of_sales, da_of_sales,
 	assumptions = {}
 	assumptions['Tax Rate']               = float(tax_rate)/100.0
 	assumptions['Growth Rate 1 year out'] = float(growth_rate_1_year_out)/100.0
-	assumptions['SGA % of sales']         = float(sga_%_of_sales)/100.0
-	assumptions['D&A % of sales']         = float(d&a_%_of_sales)/100.0
-	assumptions['CAPEX % of sales']       = float(capex_%_of_sales)/100.0
-	assumptions['NWC % of sales']         = float(nwc_%_of_sales)/100.0
+	assumptions['SGA % of sales']         = float(sga_of_sales)/100.0
+	assumptions['D&A % of sales']         = float(da_of_sales)/100.0
+	assumptions['CAPEX % of sales']       = float(capex_of_sales)/100.0
+	assumptions['NWC % of sales']         = float(nwc_of_sales)/100.0
 	assumptions['Exit Multiple']          = float(exit_multiple)
 
 	return calc_dcf()
