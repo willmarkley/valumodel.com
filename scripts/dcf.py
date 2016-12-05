ec0f70b1a6bfd88b7aa3f8792c8d3363fa18f0de
Will Markley <willjmarkley@gmail.com>
2016-12-04 23:23:19 -0500

    User adds stock ticker

--- scripts/dcf.py
+++ scripts/dcf.py
@@ -4,8 +4,10 @@ import sys
 sys.path.append('/var/www/html/valumodel.com/scripts')
 from calc_dcf import calc_dcf
 
-def create_dcf(req, tax_rate, growth_rate_1_year_out, sga_of_sales, da_of_sales, capex_of_sales, nwc_of_sales, exit_multiple):
-	ticker = 'AAPL'
+def create_dcf(req, tax_rate, growth_rate_1_year_out, sga_of_sales, da_of_sales, capex_of_sales, nwc_of_sales, exit_multiple, ticker):
+
+	### Need to error check user input
+
 	assumptions = {}
 	assumptions['Tax Rate']               = float(tax_rate)/100.0
 	assumptions['Growth Rate 1 year out'] = float(growth_rate_1_year_out)/100.0
