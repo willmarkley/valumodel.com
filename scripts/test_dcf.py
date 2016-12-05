8e414545c2e6e44ca410c449d10d81e95f770d2c
Will Markley <willjmarkley@gmail.com>
2016-12-04 21:52:29 -0500

    testing file

---
+++ scripts/test_dcf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('/var/www/html/valumodel.com/scripts')
+from calc_dcf import calc_dcf
+
+
+ticker = 'AAPL'
+assumptions = {}
+assumptions['Tax Rate']               = 0.07
+assumptions['Growth Rate 1 year out'] = 0.07
+assumptions['SGA % of sales']         = 0.07
+assumptions['D&A % of sales']         = 0.07
+assumptions['CAPEX % of sales']       = 0.07
+assumptions['NWC % of sales']         = 0.07
+assumptions['Exit Multiple']          = 0.07
+
+print calc_dcf(assumptions, ticker)
