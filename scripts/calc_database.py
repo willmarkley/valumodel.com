98149b9358462e3bd7b2d940af15c0bbb912fc32
Will Markley <willmarkley@users.noreply.github.com>
2016-12-22 21:24:54 -0500

    Update calc_database.py

--- scripts/calc_database.py
+++ scripts/calc_database.py
@@ -8,13 +8,13 @@ def calc_database(ticker):
 	fcf = {}
 	output = subprocess.check_output(['python', '/var/www/html/valumodel.com/scripts/calc_quandl.py', ticker])
 	
-	curr_year = int(output.split("^^")[0])
+	curr_year = int(output.split('^^')[0])
+	output = output.split('^^')[1]
 	
-	if curr_year is '-1':
-		print output.split("^^")[1]
+	if curr_year==-1:
+		print output
 		sys.exit()
 	else:
-		output = output.split("^^")[1]
 		for key, value in yaml.load(output).iteritems():
 			fcf[key] = value
 			
