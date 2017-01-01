9c4785795d01bb9540d7a682553265cb6332cbc4
Will Markley <willmarkley@users.noreply.github.com>
2017-01-01 17:04:30 -0500

    Update mysql_statements.py

--- scripts/mysql_statements.py
+++ scripts/mysql_statements.py
@@ -5,6 +5,5 @@ import mysql.connector
 def mysql_statements():
     cnx = mysql.connector.connect(user='root', password='$$Jasper19',host='127.0.0.1',database='valumodel')
     cnx.close()
-    return NONE
 
 
