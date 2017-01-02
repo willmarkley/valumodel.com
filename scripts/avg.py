244435f67b38e83243ace5dad01e5cf9f97ba750
Will Markley <willjmarkley@gmail.com>
2017-01-02 17:50:01 -0500

    call database

---
+++ scripts/avg.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import mysql.connector
+from jinja2 import Environment, FileSystemLoader
+
+def avg(req):
+	cnx = mysql.connector.connect(user='root', password='$$Jasper19', host='127.0.0.1', database='valumodel')
+    cursor = cnx.cursor()
+    
+    ## SELECT from database
+    cursor.execute('SELECT * FROM avg')
+    
+    totals = {}
+    times = {}
+    for (ticker, enterprise_val) in cursor:
+    	totals[ticker] += enterprise_val
+    	times[ticker] = times[ticker]+1
+    	
+    results = {}
+    for ticker in totals.keys():
+    	results[ticker] = totals[ticker] / times[ticker]
+    
+    sorted_tickers = sorted(results.keys())
+    
+    cursor.close()
+    cnx.close()
+
+	env = Environment(loader = FileSystemLoader('/var/www/html/valumodel.com/html/templates'))
+	template = env.get_template('avg.html')
+	return template.render(results=results, tickers=sorted_tickers)
