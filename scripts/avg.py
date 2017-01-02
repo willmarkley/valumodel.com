52e44d063397d9199568cf4aacaeb79136324d8f
Will Markley <willmarkley@users.noreply.github.com>
2017-01-02 17:56:42 -0500

    Update avg.py

--- scripts/avg.py
+++ scripts/avg.py
@@ -5,25 +5,25 @@ from jinja2 import Environment, FileSystemLoader
 
 def avg(req):
 	cnx = mysql.connector.connect(user='root', password='$$Jasper19', host='127.0.0.1', database='valumodel')
-    cursor = cnx.cursor()
+	cursor = cnx.cursor()
     
-    ## SELECT from database
-    cursor.execute('SELECT * FROM avg')
+	## SELECT from database
+	cursor.execute('SELECT * FROM avg')
     
-    totals = {}
-    times = {}
-    for (ticker, enterprise_val) in cursor:
-    	totals[ticker] += enterprise_val
-    	times[ticker] = times[ticker]+1
+	totals = {}
+	times = {}
+	for (ticker, enterprise_val) in cursor:
+		totals[ticker] += enterprise_val
+		times[ticker] = times[ticker]+1
     	
-    results = {}
-    for ticker in totals.keys():
-    	results[ticker] = totals[ticker] / times[ticker]
+	results = {}
+	for ticker in totals.keys():
+		results[ticker] = totals[ticker] / times[ticker]
     
-    sorted_tickers = sorted(results.keys())
+	sorted_tickers = sorted(results.keys())
     
-    cursor.close()
-    cnx.close()
+	cursor.close()
+	cnx.close()
 
 	env = Environment(loader = FileSystemLoader('/var/www/html/valumodel.com/html/templates'))
 	template = env.get_template('avg.html')
